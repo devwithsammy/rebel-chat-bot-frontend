4fb71f013085a093ffae9cf1d8883b6164941d66
samuel <devwithsammy@gmail.com>
2025-10-27 21:18:35 +0100

    fixed sidebar bug

--- src/ui/atoms/sidebarButtons.tsx
+++ src/ui/atoms/sidebarButtons.tsx
@@ -15,7 +15,7 @@ export const SidebarTogglerBtn = ({
 
     return (
   <button
-    className={`cursor-pointer dark:text-slate-100 text-slate-800 hover:outline-1 outline-offset-2 rounded-lg  outline-gray-400 w-min ${sidebarPosition=='right' ? 'rotate-180':''}`}
+    className={`cursor-pointer p-1 dark:text-slate-100 text-slate-800 hover:outline-1 outline-offset-2 rounded-lg  outline-gray-400 w-min ${sidebarPosition=='right' ? 'rotate-180':''}`}
     title={title}
     onClick={clickHandler}
   >
