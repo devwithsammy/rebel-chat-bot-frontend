6633dc2db414a251acef4cf3c9de88dd181374d6
samuel <devwithsammy@gmail.com>
2025-10-30 22:51:05 +0100

    fixd suspense bug

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,19 @@
-import { Metadata } from 'next'
-import { defaultMeta } from '@src/shared/meta'
-import { AuthTemplate } from '@src/ui/templates/AuthTemplate'
+import { Metadata } from "next";
+import { defaultMeta } from "@src/shared/meta";
+import { AuthTemplate } from "@src/ui/templates/AuthTemplate";
+import { Suspense } from "react"; 
 
-export const metadata:Metadata = { 
-    ...defaultMeta, 
-    title : `Login RebelAI`,
-    description : 'Login to your account at RebelAI'
-}
-export default function Page() { 
-    return <AuthTemplate />
+export const metadata: Metadata = {
+  ...defaultMeta,
+  title: `Login RebelAI`,
+  description: "Login to your account at RebelAI",
+};
+
+
+export default function Page() {
+  return (
+    <Suspense fallback={null}>
+      <AuthTemplate />
+    </Suspense>
+  );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,20 @@
 import { Metadata } from 'next'
 import { defaultMeta } from '@src/shared/meta'
 import { AuthTemplate } from '@src/ui/templates/AuthTemplate'
+import { Suspense } from "react"; 
+
 
 export const metadata:Metadata = { 
     ...defaultMeta, 
     title : `Join RebelAI`,
     description : 'Create an account with RebelAI'
 }
-export default function Page() { 
-    return <AuthTemplate />
-}
+
+export default function Page() {
+    return (
+      <Suspense fallback={null}>
+        <AuthTemplate />
+      </Suspense>
+    );
+  }
+  
