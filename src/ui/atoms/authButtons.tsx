552557ba1be702f2a2bae7e9a20e219a17f73aa7
samuel <devwithsammy@gmail.com>
2025-10-31 20:35:52 +0100

    fixed button

--- src/ui/atoms/authButtons.tsx
+++ src/ui/atoms/authButtons.tsx
@@ -15,7 +15,7 @@ export const ContinueWithCta = ({
   <button
     onClick={handler}
     type="button"
-    className="w-full max-w-[250px] flex items-center justify-center  gap-2 tracking-wide font-nunito border-1 border-solid border-slate-200 hover-focus:border-slate-600  rounded-[30px] py-3 px-6 cursor-pointer font-medium "
+    className="w-full max-w-[250px] flex items-center justify-center  gap-2 tracking-wide font-nunito border-1 border-solid border-slate-200 hover-focus:border-slate-400  rounded-[30px] py-3 px-6 cursor-pointer font-medium "
   >
     {Icon &&<Icon />}
     <span>{label}</span>
